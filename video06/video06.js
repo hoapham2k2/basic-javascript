b789c1062fa8cc3178b9c223603c085ad4a2e414
Hoapham <20520995@gm.uit.edu.vn>
2022-08-28 19:21:14 +0700

    init

--- video06/video06.js
+++
@@ -1,8 +0,0 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-let map = arr.map((value, index) => {
-  return value / 5;
-});
-
-console.log(arr);
-console.log(map);
---
+++ video07/video07.js
@@ -0,0 +1,5 @@
+const arr = [1, 30, 4, 21, 5, 60, 3, 2, 10];
+
+arr.sort((item1, item2) => item2 - item1);
+
+console.log(arr);
